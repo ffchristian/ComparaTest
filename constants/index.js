c04b9a1c320e2e6a60bad90f89399fc3953b790d
Christian F Farias <ffchris1@gmail.com>
2018-01-05 02:56:20 -0300

    add constants file

---
+++ constants/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by chris on 1/5/2018.
+ */
+module.exports = {
+    baseDecrease: 1,
+    baseIncrease: 1,
+    maxPrice: 50,
+    minPrice: 0,
+    productNames: {
+        superSale: 'Super Sale',
+        specialFullCoverage: 'Special Full Coverage',
+        fullCoverage: 'Full Coverage',
+        megaCoverage: 'Mega Coverage',
+    }
+};
