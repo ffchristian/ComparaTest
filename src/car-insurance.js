6c70171efd992a4a98044f6cc84bcd6d13a54626
Christian F Farias <ffchris1@gmail.com>
2018-01-05 03:00:28 -0300

    add class carInsurance and aply method selectPriceRule

--- src/car-insurance.js
+++ src/car-insurance.js
@@ -1,3 +1,17 @@
 /**
  * Created by chris on 1/5/2018.
  */
+class CarInsurance {
+    constructor(products = []) {
+        this.products = products;
+    }
+    updatePrice() {
+        this.products.forEach( product =>{
+            product.selectPriceRule();
+        });
+
+        return this.products;
+    }
+}
+
+module.exports = CarInsurance;
